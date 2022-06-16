aa0878c68d239e43810da864c08a759ee53c0b96
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2022-06-16 17:24:15 +0530

    removed extra declarations in index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express()
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
 const cors = require('cors')
-const {graphqlHTTP} = require('express-graphql')
-const schema = require('./schema/schema')
 const PORT = process.env.PORT || 4000
 app.use(cors())
 
