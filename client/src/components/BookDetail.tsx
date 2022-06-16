9490c46a7efcd829e0bbc6f5c2d70c3cb337e922
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2022-06-16 22:23:58 +0530

    updated BookDetail.tsx

--- client/src/components/BookDetail.tsx
+++ client/src/components/BookDetail.tsx
@@ -21,26 +21,32 @@ const BookDetail = (props: Props) => {
             return (
                 <div>No book selected....</div>
                 )
-        }else if(data){
-            const {book} = data
-            return(
-                <div>
-                    <h2>{book.name}</h2>
-                    <p>{book.genre}</p>
-                    <p>{book.author.name}</p>
-                    <p>All books by this author:</p>
-                    <ul className='other-books'>
-                        {
-                            book.author.books.map((book: Book)=>(
-                                <li key={book.id}>{book.name}</li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            )
+        }else if(loading){
+                return <div>Loading....</div>
+            
         }else{
-            return <div>No Data Found</div>
-        }
+            if(error){
+                return <div>No Data Found</div>
+            }else{
+                
+                const {book} = data
+                return(
+                    <div>
+                        <h2>{book.name}</h2>
+                        <p>{book.genre}</p>
+                        <p>{book.author.name}</p>
+                        <p>All books by this author:</p>
+                        <ul className='other-books'>
+                            {
+                                book.author.books.map((book: Book)=>(
+                                    <li key={book.id}>{book.name}</li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                )
+            }
+        } 
     }
   return (
     <div id='book-details'>
